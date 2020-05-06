19f3c9d8dda3615817074430e96f632b9f7af1af
Zhao Yue <zhaoyuehrb@gmail.com>
2020-05-06 23:35:42 +0800

    Fix range for plot

--- TIE_enrichment_scatter/plotScatterWithVariedSize.R
+++ TIE_enrichment_scatter/plotScatterWithVariedSize.R
@@ -1,16 +1,26 @@
 library(ggplot2)
 library(ggrepel)
-df <- read.table('L24_t0.csv', sep = ',',header = T)
-ggplot(data = df, aes(x = TIS, y = ER)) + 
-  geom_point(aes(color = GeneGroup, size = size)) +
-  scale_fill_continuous() +
-  geom_text_repel(aes(label = GeneGroup), size = 3,
-                  box.padding = unit(0.4, 'lines'),
-                  point.padding = unit(0.4, 'lines'),
-                  segment.size = 0.2, segment.colour = 'grey50') +
+
+files <- list.files(path="data", pattern="*.csv", full.names=TRUE, recursive=FALSE)
+for (file in files) {
   
-  guides(color = FALSE) + 
-  ggtitle('test') +
-  theme(axis.text.y = element_text(size = 12),
-        axis.text.x = element_text(size =12),
-        plot.title = element_text(size = rel(1.5), hjust = 0.5))
+  keyword <- substr(file, 6, 11)
+  df <- read.table(file, sep = ',',header = T)
+  ggplot(data = df, aes(x = TIS, y = ER)) + 
+    ylim(-0.25,0.72) +
+    geom_point(aes(color = GeneGroup, size = size)) +
+    scale_fill_continuous() +
+    geom_text_repel(aes(label = GeneGroup), size = 3,
+                    box.padding = unit(0.4, 'lines'),
+                    point.padding = unit(0.4, 'lines'),
+                    segment.size = 0.2, segment.colour = 'grey50') +
+    
+    guides(color = FALSE) + 
+    ggtitle(keyword) +
+    theme(axis.text.y = element_text(size = 12),
+          axis.text.x = element_text(size =12),
+          plot.title = element_text(size = rel(1.5), hjust = 0.5))
+  ggsave(paste(keyword,'ER vs TIS in Gene Groups.png'),width = 9, height = 7)
+
+
+}
